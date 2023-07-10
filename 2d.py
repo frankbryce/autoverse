030722f5fa32b9896bc53738a49a6e645f061e24
Frank Bryce <jonnyjack7@gmail.com>
2023-07-10 11:14:33 -0400

    Add rulesets to sustain some common gol patterns.

--- 2d.py
+++ 2d.py
@@ -12,7 +12,7 @@ import sys
 from time import sleep
 from tqdm import tqdm
 
-r, c, cnt = 64, 128, 4096
+r, c, cnt = 32, 64, 1024
 
 class Autoverse2D:
     def __init__(self):
@@ -25,19 +25,67 @@ class Autoverse2D:
         is_alive = lambda n: n != (n&495)
 
         # gol rules
-        self.gol_rules = [1 if (is_alive(n) and 2<=ncnt(n)<=3 or
-                            not is_alive(n) and    ncnt(n)==3)
-                            else 0 for n in range(512)]
+        def symmetrize(rules):
+            for i, ru in enumerate(rules):
+                if not ru:
+                    continue
+                new_bin = int(f'{int(bin(i)[2:]):09}'[::-1],2)
+                rules[new_bin] = 1
+            return rules
+        def add(rules):
+            for i, ru in enumerate(rules):
+                if ru:
+                    self.rules[i] = 1
+        def gol_rules():
+            return [1 if (is_alive(n) and 2<=ncnt(n)<=3 or
+                      not is_alive(n) and    ncnt(n)==3)
+                      else 0 for n in range(512)]
+        def blinker_rules():
+            rules = [0] * 512
+            for x in [7,56,73,146,292,448]:
+                rules[x] = 1
+            return rules
+        def glider_rules():
+            rules = [0] * 512
+            for x in [13,22,35,52,67,88,120,137,150,176,180,208,240,278,292,344,416,448]:
+                rules[x] = 1
+            return rules
+        def space_ship_rules():
+            rules = [0] * 512
+            for x in [7,11,14,26,30,35,38,49,53,54,56,60,73,82,89,112,145,147,152,164,200,210,224,290,312,368,392,408,416,432,448]:
+                rules[x] = 1
+            return rules
+        def glider_factory_rules():
+            rules = [0] * 512
+            for x in [7,11,13,14,19,21,22,23,25,26,27,28,29,30,35,37,38,41,42,44,49,50,51,52,53,54,56,57,67,69,73,74,76,81,82,83,84,85,86,88,89,90,92,97,100,104,112,113,114,116,120,134,137,138,145,146,147,148,149,150,152,153,154,161,162,164,168,176,178,180,193,194,200,208,209,210,212,216,224,240,259,261,262,265,268,273,274,275,276,277,278,280,281,284,289,290,292,296,304,305,306,308,312,321,324,328,336,337,338,340,344,352,368,386,392,400,401,402,404,416,432,448,464]:
+                rules[x] = 1
+            return rules
 
-        # ~1024 random spots
-        self.universe[np.random.randint(r, size=cnt),
-                      np.random.randint(c, size=cnt)] = 1
-        self.rules = list(self.gol_rules)
+        # Blinker
+        # self.universe[[10,10,10], [15,16,17]] = 1
 
-        # gol walker
-        # self.rules = gol_rules
-        # self.universe[[28,29,30,30,30], [30,31,29,30,31]] += 1
+        # Light Weight Space Ship (LWSS)
+        # self.universe[[18,18,19,20,21,21,21,21,20], [45,48,44,44,44,45,46,47,48]] = 1
 
+        # Glider
+        # self.universe[[28,29,30,30,30], [30,31,29,30,31]] = 1
+
+        # Glider Factory
+        self.universe[[4,4,5,5],[0,1,0,1]] = 1
+        self.universe[[2,2,3,3,4,4,5,5,5,5,6,6,7,7,8,8],[12,13,11,15,10,16,10,14,16,17,10,16,11,15,12,13]] = 1
+        self.universe[[0,1,1,2,2,3,3,4,4,5,5,6],[24,22,24,20,21,20,21,20,21,22,24,24]] = 1
+        self.universe[[2,2,3,3],[34,35,34,35]] = 1
+        
+        # ~cnt random spots
+        # self.universe[np.random.randint(r, size=cnt),
+        #               np.random.randint(c, size=cnt)] = 1
+
+        # add(gol_rules())
+        # add(blinker_rules())
+        # add(glider_rules())
+        # add(space_ship_rules())
+        add(glider_factory_rules())
+        self.used_rules = [0] * 512
 
     def step(self):
         tmp = scipy.ndimage.convolve(
@@ -48,6 +96,7 @@ class Autoverse2D:
     
         for i, x in np.ndenumerate(tmp):
             if self.rules[x]:
+                self.used_rules[x] = 1
                 self.universe[i] = 1
             else:
                 self.universe[i] = 0
@@ -65,6 +114,10 @@ class Autoverse2D:
                 if x == 1:
                     print('X', end='')
             print()
+        for i, x in enumerate(self.used_rules):
+            if x:
+                print(i, end=",")
+        print()
 
 def runloop():
     while True:
